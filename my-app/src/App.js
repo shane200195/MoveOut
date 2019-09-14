042948f0938b253a3fea24cd2aeb168e55f2a6cf
elisaluan <30943907+elisaluan@users.noreply.github.com>
2019-09-14 02:09:11 -0400

    dashboard base

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
 import Dashboard from "./components/dashboard/Dashboard";
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <div>
-          <Route exact path="/" component={Dashboard} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <div>
+            <Route exact path="/" component={Dashboard} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
--- my-app/src/components/dashboard/Dashboard.js
+++ my-app/src/components/dashboard/Dashboard.js
@@ -1,13 +1,58 @@
 import React, { Component } from "react";
+import "../../css/Dashboard.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
 
-function App() {
+import tempAvatar from "./static/images/avatar/1.jpg";
+
+const useStyles = makeStyles({
+  avatar: {
+    margin: 10,
+    width: 100,
+    height: 100
+  }
+});
+
+const AvatarStyles = () => {
+  const classes = useStyles();
   return (
-    <div className="Dashboard">
-      <BrowserRouter>
-        <div></div>
-      </BrowserRouter>
+    <div>
+      <Avatar alt="UserAvatar" src={tempAvatar} className={classes.avatar} />
     </div>
   );
+};
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="Dashboard">
+        <div className="User_Details" class="row">
+          <div class="col-3">
+            <AvatarStyles />
+          </div>
+          <div class="col-9">
+            <p>testing</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="container">
+            <div class="intro-text">
+              <div class="intro-lead-in">Welcome To Our Studio!</div>
+              <div class="intro-heading text-uppercase">
+                It's Nice To Meet You
+              </div>
+              <a
+                class="btn btn-primary btn-xl text-uppercase js-scroll-trigger"
+                href="#services"
+              >
+                Tell Me More
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Dashboard;
